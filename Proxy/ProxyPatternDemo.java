5b597db05022c8c9e218c505a609346cc0dbb1e6
Flyingwarts <i.gusti.tif419@polban.ac.id>
2020-07-10 23:36:48 +0700

    Proxy

---
+++ Proxy/ProxyPatternDemo.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Gusti 
+ */
+public class ProxyPatternDemo {
+	
+   public static void main(String[] args) {
+      Image image = new ProxyImage("898790887 .jpg");
+
+      //image will be loaded from disk
+      image.display(); 
+      System.out.println("");
+      
+      //image will not be loaded from disk
+      image.display(); 	
+   }
+}
